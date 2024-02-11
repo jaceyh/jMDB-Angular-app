bdf450020ae649d220757ee1a4882aad2a9a7b15
jaceyh <jacey.hallock@gmail.com>
2024-02-11 07:44:00 -0500

    add TypeDoc to .ts files in src/app

---
+++ .angular/cache/17.1.1/vite/deps/@angular_material_button.js
@@ -0,0 +1,35 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-3JN2GLCE.js";
+import "./chunk-QZMUB5VU.js";
+import "./chunk-TQPPDWFJ.js";
+import "./chunk-MFUFH4AP.js";
+import "./chunk-UKEHM6V6.js";
+import "./chunk-V2DXGMIT.js";
+import "./chunk-ZDOIMVJD.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
